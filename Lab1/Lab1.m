3cf4ba12925d0df25a5465968eafc9c992d9ef33
Alexander Poole <alex.o.poole@gmail.com>
2015-12-21 11:41:42 +0100

    Update Lab1.m

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -1,13 +1,13 @@
 %% Lab1 TSDT14
 % TODO
-% Ändra till filter istället för att annvända convolve
-% Ta fram ett simpelt tids diskret lågpass filter med 
+% Ãndra till filter istÃ¤llet fÃ¶r att annvÃ¤nda convolve
+% Ta fram ett simpelt tids diskret lÃ¥gpass filter med 
 % cuttoff Wn.
-% Räkna ut Ry1 och Ry2 med de nya tids discretra filterna
+% RÃ¤kna ut Ry1 och Ry2 med de nya tids discretra filterna
 % 
 
-cd      /edu/alepo020/skola/TSKS14/Rapport/bilder/Lab1
-addpath /edu/alepo020/skola/TSKS14/Lab1
+cd      /edu/alepo020/skola/TSDT14/Rapport/bilder/Lab1
+addpath /edu/alepo020/skola/TSDT14/Lab1
 
 %cd      /edu/annhj876/Skola/TSKS14/Rapport/bilder/Lab1
 %addpath /edu/annhj876/Skola/TSKS14/Lab1
@@ -25,14 +25,13 @@ nL = 41;
 kH = -floor(nH/2):floor(nH/2);
 kL = -floor(nL/2):floor(nL/2);
 
-thetaH = [0:1/(nH-1):1];
-thetaL = [0:1/(nL-1):1];
+thetaH = [0:1/(2*nH-2):1];
+thetaL = [0:1/(2*nL-2):1];
 
 a = 0.78;
 b1 = 1-a;
 
 a1 = [1; -a];
-
 
 
 Ry1 = 1/2*(abs(b1(1)./(1+a1(2)*exp(-1i*2*pi*thetaH))).^2);
